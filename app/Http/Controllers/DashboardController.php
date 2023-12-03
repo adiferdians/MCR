05c27ad4fa952ddd1ebec6da9e1204dc91049872
overVoid <adiferdian@outlook.com>
2023-12-03 15:41:00 +0700

    finishing dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Certificate;
+use App\Models\Client;
 
 class DashboardController extends Controller
 {
-    public function dashboard(){
-        // $countCertificate = certificate::count();
-        return view('admin.dashboard.index');
+    public function dashboard()
+    {
+        $certificationActive = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
+            ->where('clients.service', '=', 'Certification')
+            ->where('service_certifications.status', '=', 'Active')
+            ->count();
+        $certificationWithdraw = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
+            ->where('clients.service', '=', 'Certification')
+            ->where('service_certifications.status', '=', 'Withdraw')
+            ->count();
+        $certificationSuspended = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
+            ->where('clients.service', '=', 'Certification')
+            ->where('service_certifications.status', '=', 'Suspended')
+            ->count();
+
+        $consultationDone = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
+            ->where('clients.service', '=', 'Consultation')
+            ->where('service_consultations.status', '=', 'Done')
+            ->count();
+        $consultationOnProgress = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
+            ->where('clients.service', '=', 'Consultation')
+            ->where('service_consultations.status', '=', 'On Progress')
+            ->count();
+        $consultationPending = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
+            ->where('clients.service', '=', 'Consultation')
+            ->where('service_consultations.status', '=', 'Pending')
+            ->count();
+        $consultationOverdue = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
+            ->where('clients.service', '=', 'Consultation')
+            ->where('service_consultations.status', '=', 'Overdue')
+            ->count();
+
+        $certificateActive = Certificate::where('status', '=', 'Active')->count();
+        $certificateWithdraw = Certificate::where('status', '=', 'Withdraw')->count();
+        $certificateSuspended = Certificate::where('status', '=', 'Suspended')->count();
+
+
+        return view('admin.dashboard.index', [
+            'countCertificateActive' => $certificateActive,
+            'countCertificateWithdraw' => $certificateWithdraw,
+            'countCertificateSuspended' => $certificateSuspended,
+            'countCertificationActive' => $certificationActive,
+            'countCertificationWithdraw' => $certificationWithdraw,
+            'countCertificationSuspended' => $certificationSuspended,
+            'countConsultationDone' => $consultationDone,
+            'countConsultationOnProgress' => $consultationOnProgress,
+            'countConsultationPending' => $consultationPending,
+            'countConsultationOverdue' => $consultationOverdue,
+        ]);
     }
 }
