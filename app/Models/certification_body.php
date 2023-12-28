ae96a1c10059480ced4bffd1fb1249e7ea00cbb9
overVoid <adiferdian@outlook.com>
2023-12-28 19:08:34 +0700

    certificate body model

---
+++ app/Models/certification_body.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class certification_body extends Model
+{
+    use HasFactory;
+    protected $table = 'certification_bodies';
+
+    protected $guarded = [];
+}
