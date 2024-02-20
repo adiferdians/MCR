287c931258d60a6f1fcff4b6aaf4ea9a47bc7cde
overVoid <adiferdian@outlook.com>
2024-02-20 13:13:42 +0700

    Update Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,35 +10,6 @@ class DashboardController extends Controller
 {
     public function dashboard()
     {
-        // $certificationActive = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
-        //     ->where('clients.service', '=', 'Certification')
-        //     ->where('service_certifications.status', '=', 'Active')
-        //     ->count();
-        // $certificationWithdraw = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
-        //     ->where('clients.service', '=', 'Certification')
-        //     ->where('service_certifications.status', '=', 'Withdraw')
-        //     ->count();
-        // $certificationSuspended = Client::join('service_certifications', 'clients.service_id', '=', 'service_certifications.certification_id')
-        //     ->where('clients.service', '=', 'Certification')
-        //     ->where('service_certifications.status', '=', 'Suspended')
-        //     ->count();
-
-        $consultationDone = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
-            ->where('clients.service', '=', 'Consultation')
-            ->where('service_consultations.status', '=', 'Done')
-            ->count();
-        $consultationOnProgress = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
-            ->where('clients.service', '=', 'Consultation')
-            ->where('service_consultations.status', '=', 'On Progress')
-            ->count();
-        $consultationPending = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
-            ->where('clients.service', '=', 'Consultation')
-            ->where('service_consultations.status', '=', 'Pending')
-            ->count();
-        $consultationOverdue = Client::join('service_consultations', 'clients.service_id', '=', 'service_consultations.consultation_id')
-            ->where('clients.service', '=', 'Consultation')
-            ->where('service_consultations.status', '=', 'Overdue')
-            ->count();
 
         $certificateActive = Certificate::where('status', '=', 'Active')->count();
         $certificateWithdraw = Certificate::where('status', '=', 'Withdraw')->count();
@@ -48,13 +19,6 @@ class DashboardController extends Controller
             'countCertificateActive' => $certificateActive,
             'countCertificateWithdraw' => $certificateWithdraw,
             'countCertificateSuspended' => $certificateSuspended,
-            // 'countCertificationActive' => $certificationActive,
-            // 'countCertificationWithdraw' => $certificationWithdraw,
-            // 'countCertificationSuspended' => $certificationSuspended,
-            'countConsultationDone' => $consultationDone,
-            'countConsultationOnProgress' => $consultationOnProgress,
-            'countConsultationPending' => $consultationPending,
-            'countConsultationOverdue' => $consultationOverdue,
         ]);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -65,19 +65,19 @@
                             <div class="col-8 col-sm-12 col-xl-8 my-auto">
                             <div class="d-flex d-sm-block d-md-flex align-items-center card-dashboard">
                                     <h5 class="mb-0">Done</h5>
-                                    <p class="text-success ml-2 mb-0 font-weight-medium">{{$countConsultationDone}} Pcs</p>
+                                    <p class="text-success ml-2 mb-0 font-weight-medium"></p>
                                 </div>
                                 <div class="d-flex d-sm-block d-md-flex align-items-center card-dashboard">
                                     <h5 class="mb-0">On Progress</h5>
-                                    <p class="text-primary ml-2 mb-0 font-weight-medium">{{$countConsultationOnProgress}} Pcs</p>
+                                    <p class="text-primary ml-2 mb-0 font-weight-medium"></p>
                                 </div>
                                 <div class="d-flex d-sm-block d-md-flex align-items-center card-dashboard">
                                     <h5 class="mb-0">Pending</h5>
-                                    <p class="text-warning ml-2 mb-0 font-weight-medium">{{$countConsultationPending}} Pcs</p>
+                                    <p class="text-warning ml-2 mb-0 font-weight-medium"></p>
                                 </div>
                                 <div class="d-flex d-sm-block d-md-flex align-items-center card-dashboard">
                                     <h5 class="mb-0">Overdue</h5>
-                                    <p class="text-danger ml-2 mb-0 font-weight-medium">{{$countConsultationOverdue}} Pcs</p>
+                                    <p class="text-danger ml-2 mb-0 font-weight-medium"></p>
                                 </div>
                             </div>
                             <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
@@ -93,7 +93,7 @@
     <!-- partial:partials/_footer.html -->
     <footer class="footer">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©mcrcert.com 2023</span>
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©mcrcert.com 2024</span>
         </div>
     </footer>
     <!-- partial -->
